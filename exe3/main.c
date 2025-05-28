92e024a076210d16de80616630de81fa7272137b
rafa-rrayes <rafa@rayes.com.br>
2025-05-28 14:24:23 -0300

    ex3

--- exe3/main.c
+++ exe3/main.c
@@ -10,9 +10,12 @@
 #include "hardware/i2c.h"
 #include "mpu6050.h"
 
-const int I2C_CHIP_ADDRESS = 0x68;
-const int I2C_SDA_GPIO = 20;
+
+
 const int I2C_SCL_GPIO = 21;
+const int I2C_SDA_GPIO = 20;
+const int I2C_BMP_ADDRESS = 0x76;
+const int I2C_CHIP_ADDRESS = 0x68;
 
 void i2c_task(void *p) {
     i2c_init(i2c_default, 400 * 1000);
@@ -20,16 +23,13 @@ void i2c_task(void *p) {
     gpio_set_function(I2C_SCL_GPIO, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA_GPIO);
     gpio_pull_up(I2C_SCL_GPIO);
-    uint8_t buffer[1];
-    uint8_t reg_address = 0xD0;  // Registrador ID do BMP280
 
+    uint8_t buffer[6];
+    uint8_t reg_address = 0xD0;
 
-     i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, &reg_address, 1, true);
-
-     i2c_read_blocking(i2c_default, I2C_CHIP_ADDRESS, buffer, 1, false);
- 
-     printf("BMP280 ID: 0x%X \n", buffer[0]);
- 
+    i2c_write_blocking(i2c_default, I2C_BMP_ADDRESS, &reg_address, 1, true);
+    i2c_read_blocking(i2c_default, I2C_BMP_ADDRESS, buffer, 1, false);
+    printf("BMP280 ID: 0x%X \n", buffer[0]);
 
     while (1) {
         vTaskDelay(pdMS_TO_TICKS(200));
@@ -39,7 +39,7 @@ void i2c_task(void *p) {
 int main() {
     stdio_init_all();
 
-    xTaskCreate(i2c_task, "i2c task", 4095, NULL, 1, NULL);
+    xTaskCreate(i2c_task, "task 1", 4095, NULL, 1, NULL);
     vTaskStartScheduler();
 
     while (true) {
